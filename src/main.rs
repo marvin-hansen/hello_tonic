9b5b734e2c4d0f229faca7b04c0dc3637f7d9abd
Abhyudit Jain <abhyuditjain@gmail.com>
2023-08-31 16:04:23 +0530

    initial

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+mod hello_world;
+
+use crate::greeter_server::{Greeter, GreeterServer};
+use hello_world::*;
+use tonic::{transport::Server, Request, Response, Status};
+
+#[derive(Default)]
+pub struct GreeterService;
+
+#[tonic::async_trait]
+impl Greeter for GreeterService {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<HelloResponse>, Status> {
+        let name = request.into_inner().name;
+        let reply = hello_world::HelloResponse {
+            message: format!("Hello, {}!", name),
+        };
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "127.0.0.1:8080".parse()?;
+    let greeter = GreeterService;
+
+    println!("Server listening on {}", addr);
+
+    Server::builder()
+        .add_service(GreeterServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
